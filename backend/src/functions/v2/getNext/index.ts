a5cbbf4c197b74b3ff951f45a09623fb50b7dbb2
Vjeko <vjeko@vjeko.com>
2021-09-29 06:41:45 +0200

    Documenting goals for v2/getNext

--- backend/src/functions/v2/getNext/index.ts
+++ backend/src/functions/v2/getNext/index.ts
@@ -4,6 +4,34 @@ import { GetNextBindings, GetNextRequest, GetNextResponse } from "./types";
 const getNext = new AzureFunctionRequestHandler<GetNextRequest, GetNextResponse, GetNextBindings>(async (request, bindings) => {
     const { type } = request;
 
+    /*
+        Expected input:
+            {
+                appId: string,
+                [authKey: string,]
+                ranges: Range | Range[],
+                request: {
+                    type: string,
+                    count: number,
+                    perRange: boolean;
+                }[]
+            }
+
+        Output:
+            {
+                appId: string,
+                response {
+                    type: string,
+                    range: Range,
+                    ids: number[],
+                    success: boolean, // Indicates whether the requested number of ids was successfully retrieved from the specified range
+                }[]
+            }
+
+        Goals:
+        1. Read everything from a single back-end file named `<appId>.json` (this means: authorization, ranges, consumption)
+    */
+
     if (bindings) {
         //;
     }
