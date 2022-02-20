3cd2ff21e3425bf15da3763c26ff42efe1b156ca
Vjeko <vjeko@vjeko.com>
2022-02-20 10:58:07 +0100

    Fixing authorization issue with checkApp endpoint

--- azure-function-app/src/functions/v2/checkApp/index.ts
+++ azure-function-app/src/functions/v2/checkApp/index.ts
@@ -1,6 +1,8 @@
 import { ALNinjaRequestHandler } from "../ALNinjaRequestHandler";
 
 // Have to return string.toLowerCase() because returning boolean results in True/False rather than true/false
-const syncIds = new ALNinjaRequestHandler<{}, string>(async (request) => `${!!request.bindings.app}`.toLowerCase());
+const checkApp = new ALNinjaRequestHandler<{}, string>(async (request) => `${!!request.bindings.app}`.toLowerCase());
 
-export default syncIds.azureFunction;
+checkApp.skipAuthorization();
+
+export default checkApp.azureFunction;
