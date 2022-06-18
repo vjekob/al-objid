5be75b0cbbd0a874a50ee731626b88a3c30efc46
Vjeko <vjeko@vjeko.com>
2022-06-18 08:10:33 +0200

    Reconfiguring telemetry dependencies and code

--- azure-telemetry-app/telemetry/index.ts
+++ azure-telemetry-app/telemetry/index.ts
@@ -1,34 +1,12 @@
 import { RequestHandler } from "@vjeko.com/azure-func";
 import { TelemetryRequest } from "./types";
-import { createHmac } from "crypto";
-import { Https } from "./https";
+import { setup, defaultClient as client } from "applicationinsights";
+
+setup(process.env.InsightsConnectionString);
 
 const telemetry = new RequestHandler<TelemetryRequest>(async (request) => {
     const { userSha, appSha, event, context } = request.body;
-
-    const payload = { userSha, appSha, event, context };
-
-    const workspaceId = process.env["WorkspaceId"];
-    const signatureKey = process.env["IngestionSignatureKey"]
-    const contentLength = JSON.stringify(payload).length;
-    const xMsDate = new Date().toUTCString();
-    const stringToSign = `POST\n${contentLength}\napplication/json\nx-ms-date:${xMsDate}\n/api/logs`;
-    const signature = createHmac("sha256", Buffer.from(signatureKey, "base64"))
-        .update(stringToSign, "utf-8")
-        .digest("base64");
-
-    const https = new Https({
-        headers: {
-            Authorization: `SharedKey ${workspaceId}:${signature}`,
-            ["content-type"]: "application/json",
-            ["Log-Type"]: "events",
-            ["x-ms-date"]: xMsDate
-        },
-        hostname: `${workspaceId}.ods.opinsights.azure.com`,
-        path: "/api/logs?api-version=2016-04-01"
-    });
-
-    return await https.send("POST", payload);
+    client.trackEvent({ name: event, properties: { user: userSha, app: appSha, ...context }});
 });
 
 export default telemetry.azureFunction;
